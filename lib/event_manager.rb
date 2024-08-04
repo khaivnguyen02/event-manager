e3a8bdc1173278dbf431505934588858dbdbb7a5
Khai Nguyen <khaivnguyen02@gmail.com>
2024-08-04 23:45:33 +0700

    Iteration 0: Loading a file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,17 @@
 puts 'Event Manager Initialized!'
+
+# if File.exists? "event_attendees.csv"
+#   # Check if a file exists at the specified filepath through File.exist?
+#   # Read the file contents
+#   contents = File.read('event_attendees.csv')
+#   puts contents
+# end
+
+# Read the file line by line
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line, index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  p name
+end
