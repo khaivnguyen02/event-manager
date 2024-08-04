7e8119b2d51f03e319e1edc2e3ba380321022711
Khai Nguyen <khaivnguyen02@gmail.com>
2024-08-04 23:48:27 +0700

    Parsing with CSV gem

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,17 +1,14 @@
+require 'csv'
 puts 'Event Manager Initialized!'
 
-# if File.exists? "event_attendees.csv"
-#   # Check if a file exists at the specified filepath through File.exist?
-#   # Read the file contents
-#   contents = File.read('event_attendees.csv')
-#   puts contents
-# end
+contents = CSV.open(
+  'event_attendees.csv', 
+  headers: true,
+  header_converters: :symbol
+)
 
-# Read the file line by line
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  p name
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
