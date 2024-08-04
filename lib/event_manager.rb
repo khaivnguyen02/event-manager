ee63e19065932b5c6fc927dd0d16b90e6d2d9784
Khai Nguyen <khaivnguyen02@gmail.com>
2024-08-05 00:18:25 +0700

    Add begin/rescue to the API search to handle errors

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -28,12 +28,16 @@ contents = CSV.open(
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  legislators = civic_info.representative_info_by_address(
+  begin
+    legislators = civic_info.representative_info_by_address(
     address: zipcode,
     levels: 'country',
     roles: ['legislatorUpperBody', 'legislatorLowerBody']
   )
   legislators = legislators.officials
-
+  rescue 
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+  
   puts "#{name} #{zipcode} #{legislators}"
 end
